a284e387bc29c17b7a8c2ee6c6411c17ba6859ae
khalid7487 <khalid15-7487@diu.edu.bd>
2020-10-30 00:14:28 +0600

    update module

---
+++ fullstack-E-project/03-frontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+    sku: string;
+    name: string;
+    descripton: string;
+    unitPrice: number;
+    imageUrl: string;
+    active: boolean;
+    unitsInStock: number;
+    dateCreated: Date;
+    lastUpdated:Date;
+}
