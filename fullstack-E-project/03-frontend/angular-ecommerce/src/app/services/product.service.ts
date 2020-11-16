73a555debdba572cdfac6cfd98913491f03b9b4e
khalid7487 <khalid15-7487@diu.edu.bd>
2020-11-16 23:43:32 +0600

    add dynamic product menu

--- fullstack-E-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ fullstack-E-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -3,13 +3,17 @@ import { Injectable } from '@angular/core';
 import {Observable } from 'rxjs';
 import { Product } from '../common/product';
 import {map} from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
+
   private baseUrl='http://localhost:8080/api/products';
+
+  private categoryUrl='http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
   getProductList(theCategoryId: number): Observable<Product[]>{
@@ -17,15 +21,29 @@ export class ProductService {
     //need to build URL base on category id 
     const searchUrl=`${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
+    );
+
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
     );
 
   }
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded:{
     products:Product[];
+  }
+}
+
+interface GetResponseProductCategory {
+  _embedded:{
+    productCategory:ProductCategory[];
   }
 }
