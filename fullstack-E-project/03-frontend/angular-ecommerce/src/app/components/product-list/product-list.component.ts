7e7e57f561baf8af2c167987bab2912f216ca317
khalid7487 <khalid15-7487@diu.edu.bd>
2020-11-17 12:17:45 +0600

    search by keyword

--- fullstack-E-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ fullstack-E-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -12,28 +12,54 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
   currentCategoryId: number;
+  searchMode: boolean;
 
   constructor(private productService: ProductService,
-                private route: ActivatedRoute) { }
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.route.paramMap.subscribe(() =>{
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
     });
   }
 
   listProducts() {
 
-    //check if "id" parameter is available
-    const hasCategoryId: boolean=this.route.snapshot.paramMap.has('id');
+    this.searchMode= this.route.snapshot.paramMap.has('keyword');
 
-    if(hasCategoryId){
-      //get the "id" param string. convert string to a number using the "+" symbol
-      this.currentCategoryId= +this.route.snapshot.paramMap.get('id');
+    if(this.searchMode){
+      this.handleSearchProducts();
     }
     else{
+      this.handleListProducts();
+    }
+    
+  }
+
+  handleSearchProducts(){
+
+    const theKeyword:string =this.route.snapshot.paramMap.get('keyword');
+
+    //now search for the products using keyword
+    this.productService.searchProducts(theKeyword).subscribe(
+      data =>{
+        this.products =data;
+      }
+    );
+  }
+
+  handleListProducts() {
+
+    //check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      //get the "id" param string. convert string to a number using the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+    }
+    else {
       //not category id available ... default to category id 1
-      this.currentCategoryId=1;
+      this.currentCategoryId = 1;
     }
 
     //now get products for the given category id
@@ -43,5 +69,7 @@ export class ProductListComponent implements OnInit {
       }
     )
   }
+
+
 
 }
--- fullstack-E-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ fullstack-E-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -9,6 +9,7 @@ import { ProductCategory } from '../common/product-category';
   providedIn: 'root'
 })
 export class ProductService {
+  
 
 
   private baseUrl='http://localhost:8080/api/products';
@@ -16,15 +17,26 @@ export class ProductService {
   private categoryUrl='http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
+
   getProductList(theCategoryId: number): Observable<Product[]>{
 
     //need to build URL base on category id 
     const searchUrl=`${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
+    return this.getProducts(searchUrl);
+
+  }
+  searchProducts(theKeyword: string): Observable<Product[]> {
+    //need to build URL base on the keyword
+    const searchUrl=`${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+
+    return this.getProducts(searchUrl);
+  }
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
-
   }
 
   getProductCategories(): Observable<ProductCategory[]> {
