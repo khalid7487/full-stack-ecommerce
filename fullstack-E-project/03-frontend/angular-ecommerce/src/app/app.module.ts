d5a892572e67bc2f57994d48785c5a932359c5c5
khalid7487 <khalid15-7487@diu.edu.bd>
2020-11-23 22:20:22 +0600

    add shopping cart

--- fullstack-E-project/03-frontend/angular-ecommerce/src/app/app.module.ts
+++ fullstack-E-project/03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -10,8 +10,13 @@ import { ProductCategoryMenuComponent } from './components/product-category-menu
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 const routes: Routes = [
+  {path: 'checkout', component: CheckoutComponent},
+  {path: 'cart-details', component: CartDetailsComponent},
   {path: 'products/:id', component: ProductDetailsComponent},
   {path: 'search/:keyword', component: ProductListComponent},
   {path: 'category/:id', component: ProductListComponent},
@@ -27,7 +32,10 @@ const routes: Routes = [
     ProductListComponent,
     ProductCategoryMenuComponent,
     SearchComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartStatusComponent,
+    CartDetailsComponent,
+    CheckoutComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
--- fullstack-E-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ fullstack-E-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -24,6 +26,7 @@ export class ProductListComponent implements OnInit {
 
 
   constructor(private productService: ProductService,
+    private cartService: CartService,
     private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -117,4 +120,18 @@ export class ProductListComponent implements OnInit {
     this.thePageNumber=1;
     this.listProducts();
   }
+
+  addToCart(theProduct: Product){
+
+    console.log(`Adding to cart: ${theProduct.name} , ${theProduct.unitPrice}`);
+    
+    //TODO..do the real work
+    const theCartItem = new CartItem(theProduct);
+
+    this.cartService.addToCart(theCartItem);
+
+  }
+
+
+
 }
